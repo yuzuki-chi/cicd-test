bb2d5285f59fcc0a5990f815d8cb86d94783db02
yuzuki-chi <47371712+yuzuki-chi@users.noreply.github.com>
2022-06-17 13:45:29 +0900

    Update Main.java

--- test/Main.java
+++ test/Main.java
@@ -1,7 +1,5 @@
 class Main {
     public static void main(String args[]) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Hello, "+ i +" Java!");
-        }
+        System.out.println("Hello, "+ i +" Java!");
     }
 }
