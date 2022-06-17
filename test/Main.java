1370dc7e7a5ce0aa8f84cbab01d3a4d43533c223
yuzuki-chi <47371712+yuzuki-chi@users.noreply.github.com>
2022-06-17 13:40:10 +0900

    commit

--- test/Main.java
+++ test/Main.java
@@ -1,5 +1,7 @@
 class Main {
     public static void main(String args[]) {
-        System.out.println("Hello, Java!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, "+ i +" Java!");
+        }
     }
 }
